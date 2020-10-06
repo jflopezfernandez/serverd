d79d2de71dbb8a3070ad9872c7d2a47cac31f14a
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-10-06 14:29:33 -0400

    Implemented temporary, select-based multi-client support

--- serverd/src/main.c
+++ serverd/src/main.c
@@ -31,8 +31,9 @@
 
 #include <netdb.h>
 
-#include <sys/types.h>
+#include <sys/select.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -48,7 +49,7 @@ int main(void)
     hints.ai_flags = AI_PASSIVE;
 
     struct addrinfo* bind_address;
-    getaddrinfo(0, "8081", &hints, &bind_address);
+    getaddrinfo(0, "8080", &hints, &bind_address);
     
     int socket_listen = socket(bind_address->ai_family, bind_address->ai_socktype, bind_address->ai_protocol);
 
@@ -71,49 +72,85 @@ int main(void)
         return EXIT_FAILURE;
     }
 
-    struct sockaddr_storage client_address;
-    socklen_t client_len = sizeof (client_address);
+    fd_set main;
+    FD_ZERO(&main);
+    FD_SET(socket_listen, &main);
+    int max_socket = socket_listen;
 
-    int socket_client = accept(socket_listen, (struct sockaddr *) &client_address, &client_len);
+    while (1) {
+        fd_set reads = main;
 
-    if (socket_client == -1) {
-        fprintf(stderr, "[Error] %s\n", strerror(errno));
-        return EXIT_FAILURE;
+        if (select(max_socket + 1, &reads, NULL, NULL, NULL) == -1) {
+            /** @todo Improve error checking */
+            fprintf(stderr, "select() failed\n");
+            return EXIT_FAILURE;
+        }
+
+        for (int i = 1; i <= max_socket; ++i) {
+            if (FD_ISSET(i, &reads)) {
+                if (i == socket_listen) {
+                    struct sockaddr_storage client_address;
+                    socklen_t client_len = sizeof (client_address);
+
+                    int socket_client = accept(socket_listen, (struct sockaddr *) &client_address, &client_len);
+
+                    if (socket_client == -1) {
+                        fprintf(stderr, "[Error] %s\n", strerror(errno));
+                        return EXIT_FAILURE;
+                    }
+
+                    FD_SET(socket_client, &main);
+
+                    if (socket_client > max_socket) {
+                        max_socket = socket_client;
+                    }
+
+                    char address_buffer[128];
+                    getnameinfo((struct sockaddr *) &client_address, client_len, address_buffer, sizeof (address_buffer), 0, 0, NI_NUMERICHOST);
+                    printf("New connection from %s\n", address_buffer);
+                } else {
+                    char request[1024];
+                    ssize_t bytes_received = recv(i, request, 1024, 0);
+
+                    printf("%s\n", request);
+
+                    if (bytes_received < 1) {
+                        FD_CLR(i, &main);
+                        close(i);
+                        continue;
+                    }
+
+                    const char* response =
+                        "HTTP/1.1 200 OK\r\n"
+                        "Connection: Close\r\n"
+                        "Content-Type: text/html\r\n"
+                        "\r\n"
+                        "<!DOCTYPE html>\n"
+                        "<html lang='en-US'>\n"
+                        "<head>\n"
+                        "    <meta charset='UTF-8'>\n"
+                        "    <meta name='viewport' content='width=device-width, initial-scale=1.0, shrink-to-fit=no'>\n"
+                        "\n"
+                        "    <title>serverd</title>\n"
+                        "\n"
+                        "    <!-- Stylesheets -->\n"
+                        "</head>\n"
+                        "<body>\n"
+                        "    <main role='main'>\n"
+                        "        <p>serverd home</p>\n"
+                        "    </main>\n"
+                        "</body>\n"
+                        "</html>\n";
+                    
+                    ssize_t bytes_sent = send(i, response, strlen(response), 0);
+
+                    FD_CLR(i, &main);
+                    close(i);
+                }
+            }
+        }
     }
 
-    char address_buffer[100];
-    getnameinfo((struct sockaddr *) &client_address, client_len, address_buffer, sizeof (address_buffer), 0, 0, NI_NUMERICHOST);
-    printf("New connection from %s\n", address_buffer);
-
-    char request[1024];
-    ssize_t bytes_received = recv(socket_client, request, 1024, 0);
-    printf("%s\n", request);
-
-    const char* response =
-        "HTTP/1.1 200 OK\r\n"
-        "Connection: Close\r\n"
-        "Content-Type: text/html\r\n"
-        "\r\n"
-        "<!DOCTYPE html>\n"
-        "<html lang='en-US'>\n"
-        "<head>\n"
-        "    <meta charset='UTF-8'>\n"
-        "    <meta name='viewport' content='width=device-width, initial-scale=1.0, shrink-to-fit=no'>\n"
-        "\n"
-        "    <title>serverd</title>\n"
-        "\n"
-        "    <!-- Stylesheets -->\n"
-        "</head>\n"
-        "<body>\n"
-        "    <main role='main'>\n"
-        "        <p>serverd home</p>\n"
-        "    </main>\n"
-        "</body>\n"
-        "</html>\n";
-    
-    ssize_t bytes_sent = send(socket_client, response, strlen(response), 0);
-
-    close(socket_client);
     close(socket_listen);
 
     return EXIT_SUCCESS;
