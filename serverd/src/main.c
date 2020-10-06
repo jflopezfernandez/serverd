f10cc6020ee0fe6a5f1c0d83dabd0f8c4eaf952a
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2020-10-06 12:14:58 -0400

    Initial commit

---
+++ serverd/src/main.c
@@ -0,0 +1,16 @@
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+
+int main(void)
+{
+    printf("%s\n", "serverd starting...");
+
+    return EXIT_SUCCESS;
+}
